b777b060f2bf73aa0cba9a47c01573db5e8fa4f5
Kelen Nguyen <manhhieu012@gmail.com>
2020-05-27 16:06:55 +0700

    Improve Texture

--- Shooting3D/Shooting3D/Shooting3D.cpp
+++ Shooting3D/Shooting3D/Shooting3D.cpp
@@ -6,6 +6,9 @@
 #include <chrono>
 #include <time.h> 
 #include <Windows.h>
+#include <assert.h>
+#include <vector>
+
 using namespace std;
 
 #include "Vars.h"
@@ -13,47 +16,120 @@ using namespace std;
 template <typename T> 
 bool InBound(T a, T b, T c) { return (a) <= (b) and (b) <= (c); }
 
+bool movableSpace(double x, double y) {
+	return InBound<double>(0, x, mapWidth - 1) and InBound<double>(0, y, mapHeight - 1) and
+		map[(int)(x)+(int)(y)* mapWidth] != WALL;
+}
+
+void debug() 
+{
+	swprintf_s(screen, screenWidth - 1, L"%3.2lf %3.2lf %3.2lf", playerX, playerY, playerAng);
+	for (int x = 0; x < mapWidth; x++)
+		for (int y = 0; y < mapHeight; y++)
+			screen[x + (y + 1) * screenWidth] = map[x + y * mapWidth];
+	
+	screen[(int)(playerX) + ((int)(playerY) + 1) * screenWidth] = PLAYER;
+
+	int moveX = (int)round(cos(playerAng));
+	int moveY = (int)round(-sin(playerAng));
+	screen[(int)((int)(playerX + (double)moveX) + ((int)(playerY + (double)moveY) + 1) * screenWidth)] = L'*';
+}
+
 void inputHandler() {
+	auto fixAng = [&]() {
+		if (playerAng >= 2 * M_PI) playerAng -= (int)(playerAng / (2. * M_PI)) * 2. * M_PI;
+		if (playerAng <= -2 * M_PI) playerAng += (int)(-playerAng / (2. * M_PI)) * 2. * M_PI;
+	};
+
+
+	double moveX = cos(playerAng) * playerSpeedInSec * deltaTimeInSec;
+	double moveY = -sin(playerAng) * playerSpeedInSec * deltaTimeInSec;
 	if (GetAsyncKeyState((unsigned short)'A') & 0x8000) {
-		playerX -= playerSpeedInSec * deltaTimeInSec;
+		if (movableSpace(playerX + moveY, playerY + moveX)) {
+			playerX += moveY;
+			playerY -= moveX;
+		}
 	}
 	if (GetAsyncKeyState((unsigned short)'D') & 0x8000) {
-		playerX += playerSpeedInSec * deltaTimeInSec;
+		if (movableSpace(playerX - moveY, playerY + moveX)) {
+			playerX -= moveY;
+			playerY += moveX;
+		}
 	}
 	if (GetAsyncKeyState((unsigned short)'W') & 0x8000) {
-		playerY -= playerSpeedInSec * deltaTimeInSec;
+		if (movableSpace(playerX + moveX, playerY + moveY)) {
+			playerX += moveX;
+			playerY += moveY;
+		}
 	}
 	if (GetAsyncKeyState((unsigned short)'S') & 0x8000) {
-		playerY += playerSpeedInSec * deltaTimeInSec;
+		if (movableSpace(playerX - moveX, playerY - moveY)) {
+			playerX -= moveX;
+			playerY -= moveY;
+		}
 	}
+	if (GetAsyncKeyState((unsigned short)'Q') & 0x8000) {
+		playerAng += playerViewSpeedInSec * deltaTimeInSec;
+		fixAng();
+	}
+	if (GetAsyncKeyState((unsigned short)'E') & 0x8000) {
+		playerAng -= playerViewSpeedInSec * deltaTimeInSec;
+		fixAng();
+	}	
 }
 
 void update() {
 	for (int x = 0; x < screenWidth; x++) {
-		double curAng = playerAng - playerView / 2. + 
+		double curAng = playerAng + playerView / 2. -
 						(double)x * (playerView / (double)screenWidth);
 
 		double moveX = cos(curAng);
-		double moveY = sin(curAng);
-
-		double curX = playerX, curY = playerY;
-		int dis = 0;
-		while (InBound(0., curX, (double) (mapWidth - 1)) and 
-			   InBound(0., curY, (double) (mapHeight - 1))) {
-
-			if (map[(int)curX + (int)curY * mapWidth] == WALL) break;
-			curX += moveX * viewUnit;
-			curY += moveY * viewUnit;
-			dis++;
-		}		
+		double moveY = -sin(curAng);
 		
-		int wallHeight = screenHeight * 2. - (screenHeight / (double) dis);
-		cout << wallHeight << ' ';
-		/*for (int y = -wallHeight / 2; y <= wallHeight / 2; y++) {
-			screen[x + (screenHeight / 2 + y) * screenWidth] = WALL;
-		}*/
-	}		
-	cout << endl;
+		int dis = 0;		
+		double curX, curY;
+		for (dis = 1; dis <= vewDistance; dis++) {			
+			curX = (double) playerX + (double)dis * viewUnit * moveX, 
+			curY = (double) playerY + (double)dis * viewUnit * moveY;
+			
+			assert(InBound<int>(0, curX, mapWidth - 1));
+			assert(InBound<int>(0, curY, mapHeight - 1));				
+			if (not movableSpace(curX, curY)) break;			
+		}		
+						
+		int nearestCornerX = round(curX);
+		int nearestCornerY = round(curY);
+
+		double deltaX = curX - (double) nearestCornerX;
+		double deltaY = curY - (double) nearestCornerY;
+		double disToCorner = sqrt(deltaX * deltaX + deltaY * deltaY);
+
+		bool isCorner = disToCorner <= aceptedCornerDistance;
+
+		double perCent = 1. - (double)dis / vewDistance;		
+		int wallHeight = (double)screenHeight * perCent;
+		/*Wall shading*/
+		for (int y = 0; y < wallHeight; y++) {			
+			int idx = (int)((double)(wallShadow.size() - 1) * perCent);
+			assert(InBound<int>(0, idx, wallShadow.size() - 1));
+			screen[x + ((screenHeight - wallHeight) / 2 + y) * screenWidth] = 
+				isCorner ? CORNER : wallShadow[idx];
+		}		
+		/*Up floor*/
+		//for (int y = 0; y < (screenHeight - wallHeight) / 2; y++) {}
+
+		/*Bottom floor*/		
+		int beginY = (screenHeight - wallHeight) / 2 + wallHeight;
+		for (int y = beginY; y < screenHeight; y++) {
+			screen[x + y * screenWidth] = L'#';
+			int dis = screenHeight - y - 1;			
+			double floorPercent = 1 - dis / (double)(screenHeight / 2);
+
+			int idx = (int)((double)(floorShadow.size() - 1) * floorPercent);
+			assert(InBound<int>(0, idx, floorShadow.size() - 1));
+			screen[x + y * screenWidth] = floorShadow[idx];
+		}
+	}			
 }
 
 void render() {
@@ -61,13 +137,18 @@ void render() {
 	WriteConsoleOutputCharacter(hConsole, screen, screenWidth * screenHeight, { 0,0 }, &dwBytesWritten);
 }
 
+
+
 int main()
 {
-	for (int i = 0; i < screenWidth * screenHeight; i++) screen[i] = SPACE;	
-	//SetConsoleActiveScreenBuffer(hConsole);	
+	cout.precision(2);
+	cout << fixed;
+	
+	SetConsoleActiveScreenBuffer(hConsole);	
 
 	double previous = clock();
 	double lag = 0.0;	
+	for (int i = 0; i < screenWidth * screenHeight; i++) screen[i] = SPACE;
 	while (true) 
 	{		
 		inputHandler();
@@ -81,11 +162,13 @@ int main()
 
 		while (lag >= CLOCKS_PER_UPDATE)
 		{
+			for (int i = 0; i < screenWidth * screenHeight; i++) screen[i] = SPACE;			
 			update();
-			lag -= CLOCKS_PER_UPDATE;
+			lag -= CLOCKS_PER_UPDATE;			
 		}
-
-		//render();
+						
+		debug();
+		render();
 	}   
 	system("pause");
 }
--- Shooting3D/Shooting3D/Vars.h
+++ Shooting3D/Shooting3D/Vars.h
@@ -6,20 +6,23 @@ double CLOCKS_PER_UPDATE = 10. * (CLOCKS_PER_SEC / 100.);
 
 const wchar_t WALL = L'#';
 const wchar_t SPACE = L' ';
+const wchar_t PLAYER = L'X';
+const wchar_t CORNER = L' ';
 
 //const int screenWidth = 152;
-const int screenWidth = 80;
-const int screenHeight = 41;
+const int screenWidth = 120;
+const int screenHeight = 32;
 wchar_t* screen = new wchar_t[screenWidth * screenHeight];
 
-const const int mapWidth = 16;
+const int mapWidth = 16;
 const int mapHeight = 16;
 
 const double playerView = M_PI / 4.;
-const double playerSpeedInSec = 1.;
+const double playerSpeedInSec = 2.;
 const double playerSpeedInMS = playerSpeedInSec * 100.;
+const double playerViewSpeedInSec = M_PI / 4.;
 const double playerVisionDistance = 16;
-double playerAng = 0;
+double playerAng = M_PI / 2;
 double playerX = (double) mapWidth / 2., playerY = (double) mapHeight / 2.;
 
 double deltaTime = 0;
@@ -27,6 +30,9 @@ double deltaTimeInSec = 0;
 double deltaTimeInMS = 0;
 
 const double viewUnit = 0.1;
+const int vewDistance = 160;
+
+const double aceptedCornerDistance = 0.1;
 
 HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
 
@@ -38,14 +44,17 @@ wstring map = []() -> wstring {
 		wstring(L"#..............#") +
 		wstring(L"#..............#") +
 		wstring(L"#..............#") +
+		wstring(L"#....#.........#") +
 		wstring(L"#..............#") +
 		wstring(L"#..............#") +
 		wstring(L"#..............#") +
 		wstring(L"#..............#") +
 		wstring(L"#..............#") +
 		wstring(L"#..............#") +
-		wstring(L"#..............#") +
-		wstring(L"#..............#") +
+		wstring(L"#.....##########") +
 		wstring(L"#..............#") +
 		wstring(L"################");
 } ();
+
+vector<short> wallShadow = { 0x2591, 0x2592, 0x2593, 0x2588 };
+vector<short> floorShadow = { ' ', '.', '-', '=', 'm', '#' };
